520984cb68122e2cb693b99d8a0f5dd2b14699f5
Evan <evanr70@gmail.com>
2021-01-02 11:04:43 +0000

    automatic commit

--- compile.py
+++ compile.py
@@ -1,5 +1,6 @@
 from glob import glob
 from pathlib import Path
+import re
 
 
 input_directory = Path("_pages")
@@ -7,10 +8,9 @@ input_directory = Path("_pages")
 # Play nicely with gh-pages
 output_directory = Path("docs")
 
-template = Path("_pages/_template.html")
-
-with template.open() as f:
-    template = f.read()
+template_directory = Path("templates")
+default_template = "home"
+template_pattern = re.compile("<!--template: (.*?)-->")
 
 input_files = list(input_directory.glob("[!_]*.html"))
 output_files = [output_directory / file.name for file in input_files]
@@ -19,6 +19,14 @@ for input_file, output_file in zip(input_files, output_files):
     print(f"{str(input_file)} ----> {str(output_file)}")
     with input_file.open() as f:
         input_data = f.read()
+    
+    template = template_pattern.match(input_data)
+    template = default_template if template is None else template.group(1)
+    template_file = (template_directory / template).with_suffix(".html")
+    print(f"Using template: {template}.")
+    
+    with template_file.open() as f:
+      template_data = f.read()
         
     output_data = template.replace("{{{CONTENT}}}", input_data)
     
