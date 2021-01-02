33f9ec9eb6b72bde206396927bcee221758057b3
Evan <evanr70@gmail.com>
2021-01-02 22:01:44 +0000

    automatic commit

--- compile.py
+++ compile.py
@@ -1,6 +1,32 @@
 from glob import glob
 from pathlib import Path
 import re
+from syntax_highlighting import highlight_html
+from bs4 import BeautifulSoup, Comment
+
+
+def add_vega_plot(soup):
+    asset_directory = Path("docs/assets")
+  
+    head = BeautifulSoup(
+        '<style>.error{color:red;}</style><script type="text/javascript" src="https://cdn.jsdelivr.net/npm//vega@5"></script><script type="text/javascript" src="https://cdn.jsdelivr.net/npm//vega-lite@4.8.1"></script><script type="text/javascript" src="https://cdn.jsdelivr.net/npm//vega-embed@6"></script>',
+        "html.parser",
+    )
+
+    soup.head.append(head)
+
+
+    vega_comments = soup.find_all(
+        string=lambda text: isinstance(text, Comment) and ("vega:" in text)
+    )
+    
+    for comment in vega_comments:
+        vega_file_name = re.match("vega: (.*?)$", comment.strip()).group(1)
+        vega_file = (asset_directory / vega_file_name).with_suffix(".html")
+        with open(vega_file) as f:
+            vega_soup = BeautifulSoup(f.read(), "html.parser")
+
+        comment.replace_with(vega_soup.body)
 
 
 input_directory = Path("_pages")
@@ -19,16 +45,27 @@ for input_file, output_file in zip(input_files, output_files):
     print(f"{str(input_file)} ----> {str(output_file)}")
     with input_file.open() as f:
         input_data = f.read()
-    
+
     template = template_pattern.match(input_data)
     template = default_template if template is None else template.group(1)
     template_file = (template_directory / template).with_suffix(".html")
     print(f"Using template: {template}.")
-    
+
     with template_file.open() as f:
-      template_data = f.read()
-        
-    output_data = template_data.replace("{{{CONTENT}}}", input_data)
-    
+        template_data = f.read()
+
+    highlighted = highlight_html(input_data)
+
+    output_data = template_data.replace("{{{CONTENT}}}", highlighted)
+
+    soup = BeautifulSoup(output_data, "html.parser")
+
+    code_css_tag = soup.new_tag("link")
+    code_css_tag.attrs["href"] = "css/pygments.css"
+    code_css_tag.attrs["rel"] = "stylesheet"
+    soup.head.append(code_css_tag)
+
+    add_vega_plot(soup)
+
     with output_file.open("w") as f:
-        f.write(output_data)
+        f.write(soup.prettify())
---
+++ syntax_highlighting.py
@@ -0,0 +1,15 @@
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+from bs4 import BeautifulSoup
+
+
+def highlight_html(html):
+    soup = BeautifulSoup(html, "html.parser")
+
+    for item in soup.find_all("code", lang="python"):
+        code_text = highlight(item.text, PythonLexer(), HtmlFormatter(wrapcode=True))
+        s2 = BeautifulSoup(code_text, "html.parser")
+        item.parent.replace_with(s2)
+
+    return str(soup)
